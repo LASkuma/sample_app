59ba534d6a12b19d7ff4d5fd2c2217e5483def86
LASkuma <jimmyx@me.com>
2013-08-16 08:52:19 -0400

    Add some static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "Home Page" do
+    it "should have the content 'Sample App'" do
+        visit '/static_pages/home'
+        expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Home Page" do
+    it "should have the basic title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
+    end
+  end
+
+  describe "Help page" do
+      it "should have the content 'Help'" do
+          visit '/static_pages/help'
+          expect(page).to have_content('Help')
+      end
+  end
+
+  describe "Help Page" do
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+      it "should have the content 'About Us'" do
+          visit '/static_pages/about'
+          expect(page).to have_content('About Us')
+      end
+  end
+
+  describe "About Page" do
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+end
